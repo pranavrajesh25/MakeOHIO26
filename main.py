1f2fa000c2fe320e1c278e991fdf8757ccdc91b5
pranavrajesh25 <pranavrajesh9@gmail.com>
2026-03-07 12:11:02 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+import pandas as pd
+import numpy as np
+import pickle  # save/load your trained model
